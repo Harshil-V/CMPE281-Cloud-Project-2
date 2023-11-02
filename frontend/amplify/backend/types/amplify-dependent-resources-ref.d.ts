9fea812c19d68740b59ded3bf800f6898fe3dbcb
Harshil-V <harshil@ualberta.ca>
2023-11-02 10:56:13 -0700

    used aws amplify coginito to add auth to the application (uses username)

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifycloudproject20a4d2e16": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
