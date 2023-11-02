96b5a77664776b11ce2f7e5e707697d89f23c9b0
Harshil-V <harshil@ualberta.ca>
2023-11-02 11:00:00 -0700

    Added Auth to App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Amplify, Auth } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
 import App from './App.jsx'
 import './index.css'
+import awsExports from './aws-exports'
+import '@aws-amplify/ui-react/styles.css'
+
+Amplify.configure(awsExports);
+Auth.configure(awsExports)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Authenticator>
+            <App />
+        </Authenticator>
+    </React.StrictMode>,
 )
