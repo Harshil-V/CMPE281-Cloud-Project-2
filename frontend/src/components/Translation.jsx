fcfa2ea301b68bdb88e629f544dc14cbab6999c2
Harshil-V <harshil@ualberta.ca>
2023-11-21 15:21:09 -0800

    Got the styling done for the translation page. Added more languges.

--- frontend/src/components/Translation.jsx
+++ frontend/src/components/Translation.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Container, Row, Col, Form, Button, FormControl, Dropdown, DropdownButton, Badge } from 'react-bootstrap';
-import '../styles.css'
+// import '../styles.css'
 import axios from 'axios';
 
 const Translation = () => {
@@ -22,6 +22,8 @@ const Translation = () => {
     const callTranslationAPI = async () => {
         console.log(`Text: ${inputText}`)
         console.log(`Code: ${selectLanguageCode}`)
+        // setSegmentation('');
+        // setAudioUrl('');
         try {
             const response = await axios.post("http://34.213.200.68:80/translate/", {
                 text: inputText,
@@ -47,7 +49,7 @@ const Translation = () => {
         } catch (error) {
             console.error("There was an error!", error)
         }
-    }   
+    }
 
     const handleTranslate = () => {
         if (inputText == '') {
@@ -94,12 +96,17 @@ const Translation = () => {
         //         </Col>
         //     </Row>
         // </Container>
-        <Container className="container" style={{ width: 800}}>
-            <Row className="justify-content-md-center m-3 p-2" style={{ backgroundColor: '#D3D3D3', borderRadius: 5}}>
+        <Container className="container" style={{ width: 800, backgroundColor: 'grey', padding: 14 }} >
+            <center>
+                <h2 style={{ paddingLeft: 20, marginTop: 15 }}>Language Translation</h2>
+            </center>
+            <Row className="justify-content-md-center m-3 p-2" style={{ backgroundColor: '#D3D3D3', borderRadius: 5 }}>
                 <Col>
+                    <h3 style={{ paddingLeft: 30 }}>Input</h3>
 
                     <Form >
-                        <Form.Group as={Row} controlId="languageDropdown" style={{ paddingLeft: 35, paddingRight: 35
+                        <Form.Group as={Row} controlId="languageDropdown" style={{
+                            paddingLeft: 35, paddingRight: 35
                         }}>
                             <Form.Label column sm={3}>
                                 Language:
@@ -110,15 +117,19 @@ const Translation = () => {
                                     title={`Language: ${selectedLanguage} - ${selectLanguageCode}`}
                                 >
                                     <Dropdown.Item eventKey="US English" onClick={() => handleLanguageChange('en-US', 'US English')}>US English</Dropdown.Item>
-                                    <Dropdown.Item eventKey="Indian English" onClick={() => handleLanguageChange('en-IN', 'Indian English')}>Indain English</Dropdown.Item>
+                                    <Dropdown.Item eventKey="Indian English" onClick={() => handleLanguageChange('en-IN', 'Indian English')}>Indian English</Dropdown.Item>
                                     <Dropdown.Item eventKey="Spanish" onClick={() => handleLanguageChange('es', 'Spanish')}>Spanish</Dropdown.Item>
                                     <Dropdown.Item eventKey="French" onClick={() => handleLanguageChange('fr', 'French')}>French</Dropdown.Item>
                                     <Dropdown.Item eventKey="Arabic" onClick={() => handleLanguageChange('ar', 'Arabic')}>Arabic</Dropdown.Item>
+                                    <Dropdown.Item eventKey="German" onClick={() => handleLanguageChange('de', 'German')}>German</Dropdown.Item>
+                                    <Dropdown.Item eventKey="Italian" onClick={() => handleLanguageChange('it', 'Italian')}>Italian</Dropdown.Item>
+                                    <Dropdown.Item eventKey="Japanese" onClick={() => handleLanguageChange('ja', 'Japanese')}>Japanese</Dropdown.Item>
+                                    <Dropdown.Item eventKey="Hindi" onClick={() => handleLanguageChange('hi', 'Hindi')}>Hindi</Dropdown.Item>
                                     {/* Add more language options as needed */}
                                 </DropdownButton>
                             </Col>
                         </Form.Group>
-                        <Form.Group as={Row} controlId="inputText" style={{ paddingLeft: 35, paddingRight: 35}}>
+                        <Form.Group as={Row} controlId="inputText" style={{ paddingLeft: 35, paddingRight: 35 }}>
                             <Form.Label column sm={3}>
                                 Enter Text:
                             </Form.Label>
@@ -132,7 +143,7 @@ const Translation = () => {
                                 />
                             </Col>
                         </Form.Group>
-                        <Form.Group as={Row} style={{ paddingLeft: 35, paddingRight: 35}}>
+                        <Form.Group as={Row} style={{ paddingLeft: 35, paddingRight: 35 }}>
                             <Col sm={{ span: 10, offset: 6 }}>
                                 <Button variant="primary" onClick={handleTranslate} style={{ marginTop: 10 }}>
                                     Translate
@@ -142,9 +153,10 @@ const Translation = () => {
                     </Form>
                 </Col>
             </Row>
-            <Row className="justify-content-md-center m-3">
+            <Row className="justify-content-md-center m-3" style={{ backgroundColor: '#D3D3D3', borderRadius: 5, paddingTop: 10}}>
                 <Col>
-                    <Form.Group controlId="outputText" style={{ paddingLeft: 35, paddingRight: 35}}>
+                    <h3 style={{ paddingLeft: 30 }}>Output</h3>
+                    <Form.Group controlId="outputText" style={{ paddingLeft: 35, paddingRight: 35 }}>
                         <Form.Label>Translated Text:</Form.Label>
                         <FormControl as="textarea" rows={3} value={outputText} readOnly />
                     </Form.Group>
@@ -156,7 +168,7 @@ const Translation = () => {
                         )}
 
                         {segmentation && (
-                            <div>Sentiment Anaysis: &nbsp;
+                            <div className="p-2" >Sentiment Analysis: &nbsp;
                                 <Badge pill bg="primary">
                                     {segmentation}
                                 </Badge>
