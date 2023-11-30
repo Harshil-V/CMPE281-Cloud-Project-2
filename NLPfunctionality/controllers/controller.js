ee84a1d731b32c1fe2b89ad567a4f23d354759ec
MohithGowdaa <57209823+MohithGowdaa@users.noreply.github.com>
2023-11-30 13:38:58 -0800

    Update controller.js

--- NLPfunctionality/controllers/controller.js
+++ NLPfunctionality/controllers/controller.js
@@ -1,82 +1,61 @@
-const config = require('../config');
 const AWS = require('aws-sdk');
 
-// Initialize AWS
-async function initializeAWS() {
-  try {
-    const secrets = await config();
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_DEFAULT_REGION 
+});
 
-    AWS.config.update({
-      accessKeyId: secrets.AWS_ACCESS_KEY_ID,
-      secretAccessKey: secrets.AWS_SECRET_ACCESS_KEY,
-      region: secrets.AWS_DEFAULT_REGION,
-    });
+const translate = new AWS.Translate();
+const Polly = new AWS.Polly();
+const Comprehend = new AWS.Comprehend();
 
-    // Initialize AWS services using fetched credentials
-    const translate = new AWS.Translate();
-    const Polly = new AWS.Polly();
-    const Comprehend = new AWS.Comprehend();
-
-    return { translate, Polly, Comprehend };
-  } catch (error) {
-    console.error('Error initializing AWS:', error);
-    throw error;
-  }
-}
-
-// Other controller functions using AWS services
 async function translateText(text, language) {
-  try {
-    const { translate } = await initializeAWS();
-
     const translateParams = {
-      Text: text,
-      SourceLanguageCode: 'auto',
-      TargetLanguageCode: language,
+        Text: text,
+        SourceLanguageCode: 'auto',
+        TargetLanguageCode: language,
     };
 
-    const translatedData = await translate.translateText(translateParams).promise();
-    return translatedData.TranslatedText;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const translatedData = await translate.translateText(translateParams).promise();
+        return translatedData.TranslatedText;
+    } catch (error) {
+        throw error;
+    }
 }
 
 async function synthesizeSpeech(translatedText) {
-  try {
-    const { Polly } = await initializeAWS();
-
     const speechParams = {
-      OutputFormat: 'mp3',
-      Text: translatedText,
-      VoiceId: 'Joanna'
+        OutputFormat: 'mp3',
+        Text: translatedText,
+        VoiceId: 'Joanna'
     };
 
-    const pollyResponse = await Polly.synthesizeSpeech(speechParams).promise();
-    return pollyResponse.AudioStream;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const pollyResponse = await Polly.synthesizeSpeech(speechParams).promise();
+        return pollyResponse.AudioStream;
+    } catch (error) {
+        throw error;
+    }
 }
 
 async function analyzeSentiment(text) {
-  try {
-    const { Comprehend } = await initializeAWS();
-
     const sentimentParams = {
-      LanguageCode: 'en',
-      Text: text
+        LanguageCode: 'en', 
+        Text: text
     };
 
-    const sentimentData = await Comprehend.detectSentiment(sentimentParams).promise();
-    return sentimentData.Sentiment;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const sentimentData = await Comprehend.detectSentiment(sentimentParams).promise();
+        return sentimentData.Sentiment;
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
-  translateText,
-  synthesizeSpeech,
-  analyzeSentiment
+    translateText,
+    synthesizeSpeech,
+    analyzeSentiment
 };
